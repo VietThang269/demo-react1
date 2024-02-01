f236ece15b0dda98a2e660809924569938d27d1d
VietThang269 <vietthangitneu@gmail.com>
2024-02-01 11:03:44 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code>  qwewqsave to reload.
-        </p>
+        <p>Hello</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-      213123123
-    </a>
+          213123123
+        </a>
       </header>
     </div>
   );
